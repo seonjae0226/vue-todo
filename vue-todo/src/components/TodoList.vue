b37b649483eeba516c9ddd5c073d322136b5e8e2
seonjae <seonjae9488@gmail.com>
2022-05-19 09:08:57 +0900

    todo

---
+++ vue-todo/src/components/TodoList.vue
@@ -0,0 +1,74 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem" class="shadow">
+            {{ todoItem }}
+            <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
+                <i class="fa-solid fa-trash-can"></i>
+            </span>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data: function(){
+        return {
+            todoItems: []
+        }
+    },
+    //컴포넌트불러올때 실행됨
+    created: function(){
+        if(localStorage.length > 0){
+            for(var i=0; i< localStorage.length; i++){
+                if(localStorage.key(i) !== 'loglevel:webpack-dev-server'){
+                    this.todoItems.push(localStorage.key(i));
+                }
+            }
+        }
+    },
+    methods: {
+        removeTodo: function(todoItem, index){
+            console.log(todoItem, index);
+            localStorage.removeItem(todoItem);
+            this.todoItems.splice(index, 1);
+        }
+    },
+}
+</script>
+
+<style>
+    ul{
+        list-style-type: none;
+        padding-left: 0px;
+        margin-top: 0;
+        text-align: left;
+    }
+    li{
+        display: flex;
+        min-height: 50px;
+        height: 50px;
+        line-height: 50px;
+        margin: 0.5rem 15px;
+        padding: 0 0.9rem;
+        background: white;
+        border-radius: 5px;
+    }
+    .removeBtn{
+        margin-left: auto;
+        color: #eb4444;
+    }
+    .checkBtn{
+        line-height: 45px;
+        color: #62acde;
+        margin-right: 5px;
+    }
+    .checkBtnCompleted{
+        color: #b3adad;
+    }
+    .textCompleted{
+        text-decoration: line-through;
+        color: #b3adad;
+    }
+</style>
