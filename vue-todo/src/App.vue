efce77e58307241b7d6c047d834a150b7bc5e448
seonjae <seonjae9488@gmail.com>
2022-05-25 11:03:37 +0900

    ES6문법으로 변환, Enhanced Object Literals

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -16,35 +16,35 @@ import TodoList from './components/TodoList.vue'
 import TodoFooter from './components/TodoFooter.vue'
 
 export default {
-  data: function() {
+  data() {
     return {
       todoItems: []
     }
   },
   methods: {
-    addOneItem: function(todoItem){
-      var obj = {completed: false, item: todoItem};
+    addOneItem(todoItem){
+      const obj = {completed: false, item: todoItem};
       localStorage.setItem(todoItem, JSON.stringify(obj));
       this.todoItems.push(obj);
     },
-    removeOneItem: function(todoItem, index){
+    removeOneItem(todoItem, index){
       localStorage.removeItem(todoItem.item);
       this.todoItems.splice(index, 1);
     },
-    toggleOneItem: function(todoItem, index){
+    toggleOneItem(todoItem, index){
       // todoItem.completed = !todoItem.completed;
       this.todoItems[index].completed = !this.todoItems[index].completed;
       localStorage.removeItem(todoItem.item);
       localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
     },
-    clearAllItem: function(){
+    clearAllItem(){
       localStorage.clear();
       this.todoItems = [];
     }
   },
-  created: function(){
+  created(){
       if(localStorage.length > 0){
-          for(var i=0; i< localStorage.length; i++){
+          for(let i=0; i< localStorage.length; i++){
               if(localStorage.key(i) !== 'loglevel:webpack-dev-server'){
                   this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
               }
@@ -52,10 +52,10 @@ export default {
       }
   },
   components: {
-    'TodoHeader' : TodoHeader,
-    'TodoInput' : TodoInput,
-    'TodoList' : TodoList,
-    'TodoFooter' : TodoFooter
+    TodoHeader,
+    TodoInput,
+    TodoList,
+    TodoFooter
   }
 }
 </script>
--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -17,14 +17,14 @@
 import AlertModal from './common/AlertModal.vue'
 
 export default {
-    data: function() {
+    data() {
         return{
             newTodoItem: "",
             showModal: false
         }
     },
     methods: {
-        addTodo: function(){
+        addTodo(){
             if(this.newTodoItem !== ''){
                 this.$emit('addTodoItem', this.newTodoItem);
                 this.clearInput();
@@ -32,12 +32,12 @@ export default {
                 this.showModal = !this.showModal;
             }
         },
-        clearInput: function(){
+        clearInput(){
             this.newTodoItem = '';
         }
     },
     components: {
-        AlertModal: AlertModal
+        AlertModal
     }
 }
 </script>
